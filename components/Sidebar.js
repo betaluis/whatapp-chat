9945db8ac881072f12fc0e1a6fe81a8fb3db43f2
betaluis <luis.betancourt.dev@gmail.com>
2022-05-10 16:30:21 -0500

    sidebar header and search components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,12 +1,36 @@
 import styled from 'styled-components';
 
+// Icons
+import { Avatar, Button, IconButton } from '@mui/material';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+
 const Sidebar = () => {
     
     return ( 
         <Container>
             <Header>
-
+                <UserAvatar />
+                <IconsContainer>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                </IconsContainer>
             </Header>
+            <Search>
+                <SearchIcon />
+                <SearchInput placeholder="Search in chats" />
+            </Search>
+            <SidebarButton>Start a new chat</SidebarButton>
+
+            {/* List of Chats */}
+            
+
+
         </Container>
      );
 }
@@ -15,8 +39,50 @@ export default Sidebar;
 
 const Container = styled.div`
 
-` 
+`;
 
 const Header = styled.div`
+    display: flex;
+    position: sticky;
+    top: 0;
+    left: 0;
+    background-color: white;
+    z-index: 1;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    height: 80px;
+    border-bottom: 1px solid whitesmoke;
+`;
+const UserAvatar = styled(Avatar)`
+    cursor: pointer;
+    :hover {
+        opacity: 0.8;
+    }
+`;
+
+const IconsContainer = styled.div`
     
-`
+`;
+
+const Search =styled.div`
+   display: flex; 
+   align-items: center;
+   padding: 20px;
+   border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+    outline-width: 0;
+    border: none;
+    flex: 1;
+`;
+
+const SidebarButton = styled(Button)`
+    width: 100%;
+    &&& {
+        border-top: 1px solid whitesmoke;
+        border-bottom: 1px solid whitesmoke;
+        color: black;
+    }
+`;
